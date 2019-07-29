84a48934c8cc28333b39fdb8e7e8d7125e7f0cf9
Kaushal Shah <kaushal.shah@CDSYS.LOCAL>
2019-07-29 15:00:58 +0530

    changed readme

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setuptools.setup(
     name="snowcomp",
     packages=['snowcomp'],
     package_dir={'snowcomp': 'snowcomp'},
-    version="0.1.2",
+    version="0.1.3",
     author="kaushal28",
     entry_points={'console_scripts': ['snowcomp = snowcomp.__main__:main' ]},
     author_email="shah.kaushal95@gmail.com",
